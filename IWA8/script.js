39b501b31c9ca394da30c526a53b9ea8b0e003cc
TheCyberAyo <ndamaseayongezwa@gmail.com>
2023-04-08 02:12:45 +0200

    did version two of IWA8

--- IWA8/script.js
+++ IWA8/script.js
@@ -13,6 +13,64 @@ const sarahPostal = '0310'
 
 // Only change below this line
 
+/*
+ const leo ={
+	name = leoName + leoSurname
+	balance = leoBalance
+	access id = 47afb389-8014-4d0b-aff3-e40203d2107f
+	age = 24
+	address {
+		number = leoNumber
+		street = leoStreet
+		postal-code = leoPostal
+	}
+}
+
+const sarah {
+	name = saraName + saraSurname
+	age = 62
+	access id = 6b279ae5-5657-4240-80e9-23f6b635f7a8
+	balance = sarahBalance
+	address {
+		number = sarahNumber
+		street = sarahStreet
+		postal-code = sarahPostal
+	}
+
+console.log(leo, leo[address][postal-code])
+console.log(sarah, sarah[address][postal-code]) */
+
+const leo = {
+	leoName,
+	leoBalance,
+    age: 24,
+	accessId: '47afb389-8014-4d0b-aff3-e40203d2107f',
+	address: {
+		leoNumber,
+		leoStreet,
+		leoPostal,
+	}
+}
+
+
+const sarah = {
+	sarahName,
+    sarahSurname,
+    sarahBalance,
+	Age: 62,
+	accessId: '6b279ae5-5657-4240-80e9-23f6b635f7a8',
+	address: {
+		sarahNumber,
+		sarahStreet,
+		sarahPostal,
+	}
+}
+
+console.log(leo)
+console.log(sarah) 
+
+/*
+
 const leoAccessId = '47afb389-8014-4d0b-aff3-e40203d2107f'
 const leoAge = 24
 
@@ -29,4 +87,5 @@ const sarah = `${sarahName}\n${sarahSurname}\n${sarahAge}\n${sarahBalance}\n${sa
 	
 
 console.log(leo)
-console.log(sarah)
+console.log(sarah) 
+*/
